dd411008f3a9836e44676e471f60a5972dd7acdb
shalawkarim <shalaw@glaciermediadigital.ca>
2022-09-24 01:17:51 -0700

    first version

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+console.log(`
+👋 I'm Shalaw, a web developer focusing on frontend & tea. 
+I'm a passionate self-taught web developer with years of experience, 
+focusing on the frontend, especially Javascript & React. 
+Unlike the stereotype, I do not want to write "beautiful code", 
+I rather want to work with people on creative and innovative 
+projects. It does not matter how we get it through, it's 
+important that we ship functional and and user-friendly products 
+that benefit people.
+
+📖 Github: https://github.com/shalawfatah
+👨‍🎓 LinkedIn: https://linkedin.com/in/shalawfatah
+🪐 Website: https://shalaw.tech
+
+`)
