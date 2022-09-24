82496c4407a52618e1c864064c9a4e8e686cec55
shalawkarim <shalaw@glaciermediadigital.ca>
2022-09-24 14:02:12 -0700

    added color

--- index.js
+++ index.js
@@ -16,14 +16,10 @@ welcome({
 })
 
 console.log(`
-👋 I'm Shalaw, a web developer focusing on ${chalk.blue('frontend')} & ${chalk.green('green')}. 
+👋 I'm Shalaw, a web developer focusing on ${chalk.bgBlue.bold(' frontend ')} & ${chalk.bgGreen.bold(' tea ')}. 
 I'm a passionate self-taught web developer with years of experience, 
 focusing on the frontend, especially Javascript & React. 
-Unlike the stereotype, I do not want to write "beautiful code", 
-I rather want to work with people on creative and innovative 
-projects. It does not matter how we get it through, it's 
-important that we ship functional and and user-friendly products 
-that benefit people.
+${chalk.italic("It's important that we ship functional and and user-friendly products that benefit people.")}
 
 📖 Github: https://github.com/shalawfatah
 👨‍🎓 LinkedIn: https://linkedin.com/in/shalawfatah
